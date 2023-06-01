a7a988146303da074442e41967cad2092f26d92d
肥鸽子 <1214907360@qq.com>
2023-06-01 15:11:27 +0800

    header

--- component/Header.jsx
+++ component/Header.jsx
@@ -1,8 +1,74 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import { useRouter } from 'next/router';
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
+const items = [
+  {
+    label: 'Home',
+    key: 'Home',
+    icon: <MailOutlined />,
+  },
+  {
+    label: 'Test',
+    key: 'Test',
+    icon: <AppstoreOutlined />,
+  },
+  {
+    label: 'Navigation Three - Submenu',
+    key: 'SubMenu',
+    icon: <SettingOutlined />,
+    children: [
+      {
+        type: 'group',
+        label: 'Item 1',
+        children: [
+          {
+            label: 'Option 1',
+            key: 'setting:1',
+          },
+          {
+            label: 'Option 2',
+            key: 'setting:2',
+          },
+        ],
+      },
+      {
+        type: 'group',
+        label: 'Item 2',
+        children: [
+          {
+            label: 'Option 3',
+            key: 'setting:3',
+          },
+          {
+            label: 'Option 4',
+            key: 'setting:4',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: (
+      <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+        Navigation Four - Link
+      </a>
+    ),
+    key: 'alipay',
+  },
+];
 function Header() {
+  const [current, setCurrent] = useState('Home');
+  const router = useRouter();
+  const onClick = (e) => {
+    console.log('click ', e);
+    router.push(e.key);
+    setCurrent(e.key);
+  };
   return (
-    <div>Header</div>
+    <div>
+      <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
+    </div>
   )
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,75 +1,14 @@
 import '@/styles/globals.css'
+import '@/styles/style.scss'
 import type { AppProps } from 'next/app'
 import '../styles/style.scss'
-import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
-import { Menu } from 'antd';
 import { useState } from 'react';
 import Layout from '@/component/Layout'
-const items = [
-  {
-    label: 'Navigation One',
-    key: 'mail',
-    icon: <MailOutlined />,
-  },
-  {
-    label: 'Navigation Two',
-    key: 'app',
-    icon: <AppstoreOutlined />,
-    disabled: true,
-  },
-  {
-    label: 'Navigation Three - Submenu',
-    key: 'SubMenu',
-    icon: <SettingOutlined />,
-    children: [
-      {
-        type: 'group',
-        label: 'Item 1',
-        children: [
-          {
-            label: 'Option 1',
-            key: 'setting:1',
-          },
-          {
-            label: 'Option 2',
-            key: 'setting:2',
-          },
-        ],
-      },
-      {
-        type: 'group',
-        label: 'Item 2',
-        children: [
-          {
-            label: 'Option 3',
-            key: 'setting:3',
-          },
-          {
-            label: 'Option 4',
-            key: 'setting:4',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    label: (
-      <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-        Navigation Four - Link
-      </a>
-    ),
-    key: 'alipay',
-  },
-];
+
 export default function App({ Component, pageProps }: AppProps) {
-  const [current, setCurrent] = useState('mail');
-  const onClick = (e) => {
-    console.log('click ', e);
-    setCurrent(e.key);
-  };
+  
   return (
     <>
-      <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
       <Layout>
         <Component {...pageProps} />
       </Layout>
